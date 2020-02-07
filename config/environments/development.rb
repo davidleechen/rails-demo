b47f112b134cdb8d08893f1ba950f98e0825beb9
David Chen <david@chenware.com>
2020-02-06 20:01:18 -0800

    Allow Docker network range in web console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow web_console from a Docker container network
+  config.web_console.whitelisted_ips = '172.24.0.0/16'
 end
